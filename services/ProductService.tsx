4906ce658290912bd5615df955335be318e89bf0
Alexis Londoño <alexis.londono@viliv.com.co>
2022-04-28 14:35:43 -0500

    Change url services

--- services/ProductService.tsx
+++ services/ProductService.tsx
@@ -1,13 +1,15 @@
 import { Product } from "../types/Product.types";
 
 export const getAllService = async (): Promise<{ result: Array<Product> }> =>
-  fetch("http://localhost:8080/api/v1/image-market")
+  fetch("https://webserver-node-image-market.herokuapp.com/api/v1/image-market")
     .then((resp) => resp.json())
     .catch((err) => console.log(err));
 
 export const searchProduct = (
   input: string
 ): Promise<{ result: Array<Product> }> =>
-  fetch(`http://localhost:8080/api/v1/image-market/search?search=${input}`)
+  fetch(
+    `https://webserver-node-image-market.herokuapp.com/api/v1/image-market/search?search=${input}`
+  )
     .then((resp) => resp.json())
     .catch((err) => console.log(err));
